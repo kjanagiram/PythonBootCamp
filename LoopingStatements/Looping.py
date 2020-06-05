9f190811a7842261062194c0f22dde061f28085a
janakiraman <kjanagiram@gmail.com>
2020-06-06 00:18:19 +0530

    while loop

--- LoopingStatements/Looping.py
+++ LoopingStatements/Looping.py
@@ -44,3 +44,53 @@ for key,value in dictionary.items():
 
 for value in dictionary.values():
     print(value)
+'''
+While loop Syntax
+#1 while condition:
+    statement
+#2 while condition:
+    statement
+   else:
+     statement
+'''
+x=0
+while x<5:
+    print(x)
+    x+=1
+
+print()
+y=5
+while y>1:
+    print(y)
+    y-=1;
+else:
+    print(y)
+
+num=123
+sum=0
+r=0
+print()
+while num!=0:
+    r=num%10
+    sum+=r
+    num=num//10
+
+print(sum)
+
+'''
+break statement -  used to break a loop
+pass statement  -  do nothing 
+continue statement - continue the loop from the beginning
+'''
+x=0;
+while x<6:
+    if x==2:
+        x+=1
+        continue
+    if x==4:
+        break
+    print(x)
+    x+=1
+else :
+    pass
+    print("end")
